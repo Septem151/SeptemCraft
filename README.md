174831a6690c145421acd5fa9b51af904313cbb7
Carson Mullins <carsonmullins@yahoo.com>
2023-07-24 19:28:40 -0500

    Change things up a bit

---
+++ README.md
@@ -0,0 +1,21 @@
+**SeptemCraft** is a highly opinionated **Forge** modpack for Minecraft version **1.7.10**. This modpack introduces linear ore progression consisting of Vanilla, Thermal, and Tinkers ores, as well as restricts the usage of tools to solely Tinker Tools. Additionally, a ton of recipe modifications for Thermal Expansion machinery and Tinkers alloys are included to make processing progression feel meaningful.
+
+_NOTE: This modpack is in BETA and **does not** have all features implemented_
+
+## Features
+
+Features implemented in current release:
+
+- Vanilla tools disabled _(compatibility for non-tinkers pickaxes not added yet)_
+  - **Ore progression** drastically changed:
+  - _Stones_
+  - _Woods_
+  - _Coal, Tin, Copper, Aluminum_
+  - _Bronze, Iron, Invar, Electrum, Gold, Redstone, Silver, Lead, Lapis_
+  - _Steel, Diamonds, Obsidian, Emeralds_
+  - _Alumite, Cobalt, Ardite_
+  - _Manyullyn_
+  - _Platinum, Certus Quartz_
+- Thermal Expansion recipes modified to be in-line with ore progression and more resource intensive
+- Tinkers Construct alloy changes for Alumite and Signalum
+- Vanilla furnace cannot smelt ores
--- download_mods.py
+++ download_mods.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+"""
+Downloads mods from CurseForge and GitHub using a special json schema.
+"""
 import json
 import os
 import shutil
@@ -7,12 +10,13 @@ from abc import ABC, abstractmethod
 from enum import Enum, StrEnum, auto
 from pathlib import Path
 from typing import Any
-from urllib.parse import unquote, urlparse
 
 import requests
 
 
 class ModType(StrEnum):
+    """Enum for valid "source" parameters in the JSON schema"""
+
     GITHUB = auto()
     CURSEFORGE = auto()
     LOCAL = auto()
@@ -20,6 +24,8 @@ class ModType(StrEnum):
 
 
 class DownloadStatus(Enum):
+    """Enum containing statuses for a dependency's download"""
+
     MISSING = auto()
     SKIPPED = auto()
     SUCCESS = auto()
@@ -27,23 +33,41 @@ class DownloadStatus(Enum):
 
 
 class Dependency(ABC):
+    """Base Dependency interface"""
+
     @property
     @abstractmethod
     def headers(self) -> dict[str, Any]:
+        """
+        Default headers for HTTP requests
+        MUST BE IMPLEMENTED BY CLASSES
+        """
         return {"Accept": "application/json"}
 
     @property
     @abstractmethod
     def source(self) -> ModType:
+        """
+        Source value for dependency
+        MUST BE IMPLEMENTED BY CLASSES
+        """
         return ModType.UNKNOWN
 
     @property
     @abstractmethod
     def jar_name(self) -> str:
-        pass
+        """
+        Jar name for dependency
+        MUST BE IMPLEMENTED BY CLASSES
+        """
 
     @abstractmethod
     def download(self, local_path: Path) -> DownloadStatus:
+        """
+        Default check to see if mod already exists.
+        Downloads the file to local_path.
+        MUST BE IMPLEMENTED BY CLASS
+        """
         mod_path = local_path / self.jar_name
         if mod_path.exists():
             print(f"Skipping download of {self.source} mod, file exists at {mod_path}")
@@ -53,19 +77,34 @@ class Dependency(ABC):
 
     @abstractmethod
     def to_json(self) -> dict[str, Any]:
+        """
+        Returns a JSON representation of the dependency.
+        MUST BE IMPLEMENTED BY CLASS
+        """
         return {"source": self.source, "data": {}}
 
 
 class GitHubDependency(Dependency):
+    """
+    JSON Schema:
+    {
+        "source": "github",
+        "data": {
+            "url": string,
+            "author": string,
+            "repo": string,
+            "asset": integer,
+            "jar_name": string
+        }
+    }
+    """
+
     api_key: str = os.getenv("GH_TOKEN", "")
     api_base_url: str = "https://api.github.com"
 
-    def __init__(
-        self, url: str, author: str, repo: str, asset: int, jar_name: str
-    ) -> None:
+    def __init__(self, url: str, repo_info: str, asset: int, jar_name: str) -> None:
         self.url = url
-        self.author = author
-        self.repo = repo
+        self.author, self.repo = repo_info.split("/", 1)
         self.asset = asset
         self._jar_name = jar_name
 
@@ -86,7 +125,10 @@ class GitHubDependency(Dependency):
         if status == DownloadStatus.SKIPPED:
             return status
         mod_path = local_path / self._jar_name
-        asset_url = f"{self.api_base_url}/repos/{self.author}/{self.repo}/releases/assets/{self.asset}"
+        asset_url = (
+            f"{self.api_base_url}/repos/{self.author}/{self.repo}"
+            "/releases/assets/{self.asset}"
+        )
         asset_request = requests.get(
             asset_url,
             timeout=5,
@@ -112,6 +154,19 @@ class GitHubDependency(Dependency):
 
 
 class CurseForgeDependency(Dependency):
+    """
+    JSON schema:
+    {
+        "source": "curseforge",
+        "data": {
+            "url": "string",
+            "project": integer,
+            "file": integer,
+            "jar_name": string
+        }
+    }
+    """
+
     api_key: str = os.getenv("CURSEFORGE_TOKEN", "")
     api_base_url: str = "https://api.curseforge.com/v1"
 
@@ -174,6 +229,17 @@ class CurseForgeDependency(Dependency):
 
 
 class LocalDependency(Dependency):
+    """
+    JSON Schema:
+    {
+        "source": "local",
+        "data": {
+            "path": string,
+            "jar_name": string
+        }
+    }
+    """
+
     def __init__(self, path: str, jar_name: str) -> None:
         self.path = path
         self._jar_name = jar_name
@@ -208,43 +274,48 @@ class LocalDependency(Dependency):
         }
 
 
-class DependencyFactory:
-    @staticmethod
-    def from_json(json_data: dict[str, Any]) -> Dependency | None:
-        source: str = json_data.get("source", ModType.UNKNOWN)
-        data: dict[str, Any] = json_data.get("data", {})
-        if source == ModType.GITHUB:
-            return GitHubDependency(
-                data["url"],
-                data["author"],
-                data["repo"],
-                data["asset"],
-                data["jar_name"],
-            )
-        if source == ModType.CURSEFORGE:
-            return CurseForgeDependency(
-                data["url"],
-                data["project"],
-                data["file"],
-                data["jar_name"],
-            )
-        if source == ModType.LOCAL:
-            return LocalDependency(data["path"], data["jar_name"])
-        return None
+def dependency_from_json(json_data: dict[str, Any]) -> Dependency:
+    """
+    Factory method to create dependency objects from JSON schema
+    """
+    source: str = json_data.get("source", ModType.UNKNOWN)
+    data: dict[str, Any] = json_data.get("data", {})
+    if source == ModType.GITHUB:
+        return GitHubDependency(
+            data["url"],
+            data["repo_name"],
+            data["asset"],
+            data["jar_name"],
+        )
+    if source == ModType.CURSEFORGE:
+        return CurseForgeDependency(
+            data["url"],
+            data["project"],
+            data["file"],
+            data["jar_name"],
+        )
+    if source == ModType.LOCAL:
+        return LocalDependency(data["path"], data["jar_name"])
+    raise RuntimeError(f"unknown source for mod: {source}")
 
 
 def main() -> None:
+    """
+    Main program entrypoint, loops through each mod
+    in the modlist.json file and downloads each dependency to the
+    mods/ folder as needed.
+    """
     download_directory = Path(__file__).resolve().parent / "mods"
     if not download_directory.exists():
         download_directory.mkdir(parents=True)
-    mod_list: list[str] = json.load(
+    mod_list: list[dict[str, Any]] = json.load(
         (Path(__file__).resolve().parent / "modlist.json").open("r", encoding="UTF-8")
     )
     success_count = 0
     skip_count = 0
     error_count = 0
     for mod_json in mod_list:
-        mod = DependencyFactory.from_json(mod_json)
+        mod = dependency_from_json(mod_json)
         result = mod.download(download_directory)
         if result == DownloadStatus.SUCCESS:
             success_count += 1
